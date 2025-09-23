7184f311fb7343a91e7aae72907b3ba8e7e009f1
nazaningh2004 <nazanin.ghanbari400@gmail.com>
2025-09-23 19:22:27 +0330

    Initial commit

--- main.js
+++ main.js
@@ -6,19 +6,17 @@ const weatherIcon = document.querySelector('.weather-icon');
 const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
 
-
-
 const apikey = '7104559e9e87fb371249e3579c68105a';
 
 async function apicall() {
   const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apikey}&units=metric`;
 
-  const response = await fetch(apiurl );
-  
+  const response = await fetch(apiurl);
+
   if (response.status == 404) {
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".weather").style.display = "none";
-}
+    document.querySelector('.error').style.display = 'block';
+    document.querySelector('.weather').style.display = 'none';
+  }
 
   var data = await response.json();
   console.log(input.value + '!!!!');
@@ -31,17 +29,13 @@ async function apicall() {
 
   if (data.weather[0].main == 'Clouds') {
     weatherIcon.src = 'images/cloudy.png';
-  } 
-  else if (data.weather[0].main == 'Clear') {
+  } else if (data.weather[0].main == 'Clear') {
     weatherIcon.src = 'images/Clear.png';
-  } 
-  else if (data.weather[0].main == 'Rain') {
+  } else if (data.weather[0].main == 'Rain') {
     weatherIcon.src = 'images/rain.png';
-  }
-   else if (data.weather[0].main == 'Drizzle') {
+  } else if (data.weather[0].main == 'Drizzle') {
     weatherIcon.src = 'images/rainsunny.png';
-  }
-   else if (data.weather[0].main == 'Mist') {
+  } else if (data.weather[0].main == 'Mist') {
     weatherIcon.src = 'images/sunny.png';
   }
 }
